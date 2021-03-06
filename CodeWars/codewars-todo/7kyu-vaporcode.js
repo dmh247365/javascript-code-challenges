8754913dcab2e6c5c8233f21fd6037a3c146866c
Dave <david.halewoodfcc@gmail.com>
2021-03-06 13:43:48 +0000

    remove moved files

--- CodeWars/codewars-todo/7kyu-vaporcode.js
+++
@@ -1,8 +0,0 @@
-// Solution 1
-
-// Solution 2
-
-
-console.log(vaporcode("Lets go to the movies") === "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S");
-console.log(vaporcode("Why isnt my code working"),"W  H  Y  I  S  N  T  M  Y  C  O  D  E  W  O  R  K  I  N  G");
-
