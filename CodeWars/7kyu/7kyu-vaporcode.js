2600067daa4840903ff2523386d153a76f8eb7f9
Dave <david.halewoodfcc@gmail.com>
2021-03-08 13:32:51 +0000

    completed vaporcode

--- CodeWars/7kyu/7kyu-vaporcode.js
+++ CodeWars/7kyu/7kyu-vaporcode.js
@@ -1,12 +1,38 @@
 // Solution 1
+function vaporcode1(text) {
+  const a = [];
+  let uppercase = text.toUpperCase().split('');
+  const map1 = uppercase.map(x => {
+    if(x !== ' ') {
+      a.push(x, "");
+    }
+  })
+  return  a.join(' ').trim();
+}
 
 // Solution 2
+function vaporcode2(text) {
+  return text.toUpperCase().split(" ").join("").split("").join("  ");
+}
+
+// Solution 3
+function vaporcode3(text) {
+  return text.toUpperCase().replace(/ /g,'').split('').join('  ');
+}
+
 
 console.log('solution-1');
-console.log(vaporcode("Lets go to the movies") === "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S");
-console.log(vaporcode("Why isnt my code working"),"W  H  Y  I  S  N  T  M  Y  C  O  D  E  W  O  R  K  I  N  G");
+console.log(vaporcode1("Lets go to the movies") === "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S");
+console.log(vaporcode1("Why isnt my code working") === "W  H  Y  I  S  N  T  M  Y  C  O  D  E  W  O  R  K  I  N  G");
 console.log("");
 
 
 console.log('solution-2');
+console.log(vaporcode2("Lets go to the movies") === "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S");
+console.log(vaporcode2("Why isnt my code working") === "W  H  Y  I  S  N  T  M  Y  C  O  D  E  W  O  R  K  I  N  G");
+console.log("");
+
+console.log('solution-3');
+console.log(vaporcode3("Lets go to the movies") === "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S");
+console.log(vaporcode3("Why isnt my code working") === "W  H  Y  I  S  N  T  M  Y  C  O  D  E  W  O  R  K  I  N  G");
 console.log("");
--- CodeWars/7kyu/7kyu-vaporcode.md
+++ CodeWars/7kyu/7kyu-vaporcode.md
@@ -13,16 +13,36 @@ vaporcode("why isn't my code working?") => "W  H  Y  I  S  N  '  T  M  Y  C  O  
 <hr>
 
 ### Approach
-Blah blah blah  
+1 - Will need to convert text to upper case
+2 - Insert space after each letter 
+3 - use regex?
 
+split() method splits a string into an array of substrings.
+join() method returns contents of an array as a string.
+toUpperCase() converts string to uppercase without mutating the original string.
+replace() method, returns a new string, based on a pattern we pass in.
 
 #### To run solutions
 just navigate to the relevant folder and type $node <filename>
 
 #### Solution 1
-
-Use 
+works but far too long winded. 
 
 #### Solution 2
+ ie string = "the red car"
 
-Use  
+1. transform original string to uppercase `THE RED CAR`
+2. convert string to an array, `['THE', 'RED', 'CAR']`
+3. join array contents into a single string with no spaces `THEREDCAR`
+4. Split string into an array by letter, `['T', 'H', 'E', 'R'...etc]`
+5. Join contents  `T  H  E  R  E  D  C  A  R`
+
+
+#### Solution 3
+ ie string = "the red car
+
+1. transform original string to uppercase `THE RED CAR`
+2. replace passing in regex global flag and also ' ' `THEREDCAR`
+7. Split string into an array by letter, `['T', 'H', 'E', 'R'...etc]`
+8. Join contents `T  H  E  R  E  D  C  A  R`
+
